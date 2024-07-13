1013e11bd30c0c2f9061422e608333ca49cbe9eb
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2024-07-12 23:26:28 -0300

    2. Home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="" className="home__social-icon" target="_blank">
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/gonzalo-quiroga7/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/GonzaloQuirogaS"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
